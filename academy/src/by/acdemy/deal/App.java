b4cd8c6e123e0a144068d862d3f460baa168686c
Velichko Maksim <mak6000.eve@gmail.com>
2021-02-16 13:24:40 +0300

    More polishing

--- academy/src/by/acdemy/deal/App.java
+++ academy/src/by/acdemy/deal/App.java
@@ -72,7 +72,7 @@ public class App {
 		if (DateValidator.getDateSlash(date)) {
 			Date dateTemp = new SimpleDateFormat("dd/MM/yyyy").parse(date);
 			deals[dealCounter].setDate(
-					"Day: " + dateTemp.getDay() + "\nMonth: " + dateTemp.getMonth() + "\nYear: " + dateTemp.getYear());
+					"Day: " + dateTemp.getDay() + "\nMonth: " + dateTemp.getMonth() + "\nYear: " + (1900 + dateTemp.getYear()));
 
 		} else if (DateValidator.getDateDash(date)) {
 			Date dateTemp = new SimpleDateFormat("dd-MM-yyyy").parse(date);
